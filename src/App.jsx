8e12c56648ba01ad79335a8c67257af65ef2fe42
Javier Mamani <javier.mamani08@inacapmail.cl>
2026-06-25 20:18:16 -0400

    Feat proyecto base con datos

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { mascotas } from './data/mascotas'
+
 
 function App() {
   const [count, setCount] = useState(0)
+  console.log('Mascotas cargadas:', mascotas)
 
   return (
     <>
